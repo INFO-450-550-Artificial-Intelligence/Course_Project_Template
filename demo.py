8e592ab7e6dec5ffb2fbf1a4121a96230284819b
Millhouse <tylermillhouse@arizona.edu>
2026-08-20 17:17:39 -0700

    Fix typo in demo.py docstring

--- demo.py
+++ demo.py
@@ -4,9 +4,9 @@ import algorithms as alg
 '''
 This file will contain the code needed to run a demonstration of your project. 
 
-For the progress report, it should contain code that shows a breif demonstration of random agents attempting to solve each problem. 
+For the progress report, it should contain code that shows a brief demonstration of random agents attempting to solve each problem. 
 
-For the final report, it should contain code that shows a breif demonstration of each algorithm attempting to solve each problem. 
+For the final report, it should contain code that shows a brief demonstration of each algorithm attempting to solve each problem. 
 
 In both cases, to make sure we can grade these all in time, please ensure that each demonstration is just long enough to show some
 meaningful behavior. If your problem is chess playing, for example, you might show 5 moves from each player, where one player uses one 
